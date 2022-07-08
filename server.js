b18b178bb52d7e0c45208acdf57195fc45cb5e65
thesouravarya <108688559+thesouravarya@users.noreply.github.com>
2022-07-08 22:53:37 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,59 @@
+const http = require('http')
+
+const express = require('express')
+
+const app = express()
+
+const socketio = require('socket.io')
+
+const PORT=process.env.PORT || 3000;
+
+const server = http.createServer(app)
+
+const io = socketio(server)
+let users = {
+    'aky1': 'aky1'
+}
+let socketmap = {}
+io.on('connection', function (socket) {
+    console.log('connected with socket with id - ', socket.id)
+
+    function login(s, u) {
+        s.join(u)
+        s.emit('logged_in')
+        socketmap[s.id] = u
+        console.log(socketmap)
+
+    }
+    socket.on('login', (data) => {
+        if (users[data.username]) {
+            if (users[data.username] == data.password) {
+                login(socket, data.username)
+            } else {
+                socket.emit('login_failed')
+            }
+
+        } else {
+            users[data.username] = data.password
+            login(socket, data.username)
+
+        }
+        console.log(users)
+    })
+
+    socket.on('msg_send', (data) => {
+        data.from = socketmap[socket.id]
+        if (data.to) {
+            io.to(data.to).emit('msg_rcvd', data)
+        } else {
+            io.emit('msg_rcvd', data)
+        }
+    })
+})
+
+app.use('/', express.static('public'))
+
+server.listen(PORT, function () {
+    console.log(` app running on ${PORT}`)
+
+})
